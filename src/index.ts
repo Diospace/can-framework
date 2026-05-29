d79baf1b01daae400764d8e9c892f6679eba5ca4
Endurance Ogun <enduranceogun6@gmail.com>
2026-05-29 19:57:56 +0100

    fix: move esbuild to dependencies for CLI runtime

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@
 export * from './runtime-helpers';
 
 // Export version or other metadata if needed
-export const VERSION = '1.0.8';
+export const VERSION = '1.0.9';
 
 // If you have a router module, export it as well:
 // export * from './router/index';
